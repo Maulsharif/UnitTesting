65c1c46531122d6260863722fe2e8a1edb5d4a8f
Madina Mauilsharipova <Madina_Mauilsharipova@epam.com>
2021-12-07 13:09:41 +0600

    Add project files.

---
+++ UnitTesting/OddEven.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTesting
+{
+   public static class OddEven
+    {
+        public static int[] GenerateHundredNumbers(int start, int end)
+        {
+            int[] result = new int[100];
+            Random rnd = new Random();
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = rnd.Next(start, end);           
+            }
+            return result;
+        }
+    }
+}
